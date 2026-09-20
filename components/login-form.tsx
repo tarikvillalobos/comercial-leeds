52f89222b5c24f4016ad6b1622051ac89c4b08a8
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-19 21:25:24 -0300

    feat: remove password recovery link

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -41,15 +41,7 @@ export function LoginForm({
                 />
               </Field>
               <Field>
-                <div className="flex items-center">
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
                 <Input id="password" type="password" required />
               </Field>
               <Field>
