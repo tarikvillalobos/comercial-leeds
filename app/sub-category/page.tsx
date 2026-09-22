a69ec9d82714e0d1be90f0067891f784ccf6afe1
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-21 22:17:15 -0300

    feat: remove placeholder subcategory route

--- app/sub-category/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link"
-
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-
-export default function SubCategoryPage() {
-  return (
-    <main className="min-h-svh p-6">
-      <div className="mx-auto w-full max-w-5xl">
